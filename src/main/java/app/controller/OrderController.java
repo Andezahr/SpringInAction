b3971a1cf17c3554d0c15168cff161887e4d72a6
Andezahr <andr.zax@gmail.com>
2025-01-29 16:56:54 +0300

    Дошёл до 72 страницы, добавил контроллер заказа и предстоит добавить форму заказа

---
+++ src/main/java/app/controller/OrderController.java
@@ -0,0 +1,19 @@
+package app.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import app.model.TacoOrder;
+
+@Slf4j @Controller @RequestMapping("/orders") @SessionAttributes("tacoOrder")
+public class OrderController {
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+}
--- src/main/java/app/model/IngredientByIdConverter.java
+++ src/main/java/app/model/IngredientByIdConverter.java
@@ -1,4 +1,28 @@
 package app.model;
 
-public class IngredientByIdConverter {
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import app.model.Ingredient;
+import app.model.Ingredient.Type;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class IngredientByIdConverter implements Converter<String, Ingredient> {
+    private Map<String, Ingredient> ingredientMap = new HashMap<>();
+
+    public IngredientByIdConverter() {
+        ingredientMap.put("FLTO", new Ingredient("FLTO", "Flour Tortolla", Type.WRAP));
+        ingredientMap.put("BEEF", new Ingredient("BEEF", "Beef", Type.PROTEIN));
+        ingredientMap.put("SLSA", new Ingredient("SLSA", "Salsa", Type.SAUCE));
+        ingredientMap.put("CHED", new Ingredient("CHED", "Cheddar", Type.CHEESE));
+        ingredientMap.put("LTCE", new Ingredient("LTCE", "Lettuce", Type.VEGGIES));
+    }
+
+    @Override
+    public Ingredient convert(String id) {
+        return ingredientMap.get(id);
+    }
 }
